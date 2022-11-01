ffeed2402a57eb2223236dd2c21ae3899732ea04
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-11-01 09:20:17 -0400

    made attractions components and some styling

--- client/src/components/Attractions.jsx
+++ client/src/components/Attractions.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import Attraction from '../components/Attraction'
 
-const Attractions = () => {
+const ViewAttractions = () => {
+  const [attractions, setAttractions] = useState([])
+
+  let navigate = useNavigate()
+  let { attractionsId } = useParams()
+
+  const getAttractions = async () => {
+    const response = await axios.get(`http://localhost:3001/attractions`)
+    setAttractions(response.data.attractions)
+    console.log(response.data.attractions)
+  }
+
+  const viewAttraction = (id) => {
+    navigate(`/attractions/${id}`)
+  }
+
+  useEffect(() => {
+    getAttractions()
+  }, [attractionsId])
+
   return (
-    <div>Attractions</div>
+    <div className="attractionGrid">
+      <div className="allAttractions">
+        {attractions
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((attraction) => (
+            <Attraction
+              id={attraction._id}
+              key={attraction._id}
+              image={attraction.image}
+              name={attraction.name}
+              onClick={viewAttraction}
+            />
+          ))}
+      </div>
+    </div>
   )
 }
 
-export default Attractions
+export default ViewAttractions
