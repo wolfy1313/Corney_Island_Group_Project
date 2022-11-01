e9d8de5f80749e96590325209318d7fcb42fbf7e
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-11-01 14:22:53 -0400

    added ThemeAttractions component

--- client/src/components/ThemeAttractions.jsx
+++ client/src/components/ThemeAttractions.jsx
@@ -1,15 +1,44 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
 import '../components/Attraction'
 
 const ThemeAttractions = () => {
-  
+  const [attractions, setAttractions] = useState([])
+
+  let navigate = useNavigate()
+  let { attractionsId } = useParams()
+  // let { themeId } = useParams()
+
+  const getThemeAttractions = async () => {
+    const response = await axios.get(`http://localhost:3001/attractions/`)
+    setAttractions(response.data.attractions)
+  }
+
+  const viewAttraction = (id) => {
+    navigate(`/attractions/${id}`)
+  }
+
+  useEffect(() => {
+    getThemeAttractions()
+  }, [attractionsId])
 
   return (
-    <div>
-
-      <div>
-
+<div className="attractionGrid">
+      <div className="allAttractions">
+        {attractions
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((attraction) => (
+            <Attraction
+              id={attraction._id}
+              key={attraction._id}
+              image={attraction.image}
+              name={attraction.name}
+              theme={attraction.theme}
+              onClick={viewAttraction}
+            />
+          ))}
       </div>
-
     </div>
   )
 }
